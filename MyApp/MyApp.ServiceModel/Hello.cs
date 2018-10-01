0db2bc5e0bd252f37450aa877886346ad2911023
Eric Munn <ericgmunn@gmail.com>
2018-10-01 09:59:09 -0700

    Sample

---
+++ MyApp/MyApp.ServiceModel/Hello.cs
@@ -0,0 +1,16 @@
+﻿using ServiceStack;
+
+namespace MyApp.ServiceModel
+{
+    [Route("/hello","GET POST")]
+    public class Hello : IReturn<HelloResponse>
+    {
+        public string Foo { get; set; }
+        public string Bar { get; set; }
+    }
+
+    public class HelloResponse
+    {
+        public Hello Result { get; set; }
+    }
+}
